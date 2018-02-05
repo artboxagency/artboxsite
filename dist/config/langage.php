a87ee3cc72a9404118c8675e2a24f06897e5a028
antoineartbox <antoine.fortin@artbox.agency>
2018-02-05 09:55:12 -0500

    Adding langage if notthing set

--- dist/config/langage.php
+++ dist/config/langage.php
@@ -1,8 +1,10 @@
 <?php
 
-
 if($_GET["lang"] == "en") {
     $_SESSION["locale"] = "en";
 } else if($_GET["lang"] == "fr") {
     $_SESSION["locale"] = "fr";
+} else if(!isset($_GET["lang"])) {
+    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    $_SESSION["locale"] = $lang;
 }
