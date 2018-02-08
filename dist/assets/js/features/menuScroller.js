aa38b971e2e77d243b094f0eb31f8b26d80bd031
antoinefortin <antoineradiohst@gmail.com>
2018-02-08 11:22:19 -0500

    Adding menu

--- dist/assets/js/features/menuScroller.js
+++ dist/assets/js/features/menuScroller.js
@@ -5,14 +5,43 @@ $( document ).ready(function() {
     $(".menu-item").click(function () {
 
         var menuLinkItem = $(this).find("a").attr("href");
-        console.log(menuLinkItem);
 
         $("html, body").animate({scrollTop:$(menuLinkItem.toLowerCase() + "-scroller").offset().top - 100});
 
 
     });
 
+
     // Decalre component
+    var menuItem = $(".menu-item");
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        for(var i = 0; i < menuItem.length; i++) {
+            $(menuItem[i]).css({"border": "none"});
+            var item = $(menuItem[i]).find("a").attr("href");
+            var state = item.includes("#");
+
+            if(state) {
+                
+                var linkPosition = $(item.toLowerCase() + "-scroller").offset().top;
+                console.log("Current link position  :   "  + linkPosition);
+                console.log(linkPosition.height);
+
+                if(scroll > (linkPosition - 75) && scroll < (linkPosition)) {
+                    $(menuItem[i]).css({"border": "solid 2px black"});
+                }
+
+            }
 
 
+
+        }
+    });
+
+
+
+
+
+
+    console.log(menuItem);
 })
