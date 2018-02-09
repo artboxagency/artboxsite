ff79ee6f7990f55a396b094fb1a05a2461036b43
antoinefortin <antoineradiohst@gmail.com>
2018-02-09 16:14:55 -0500

    Adding the front-end for Phili

--- dist/utils/chameleon-pannel/Dashboard/Controllers/EditController.php
+++ dist/utils/chameleon-pannel/Dashboard/Controllers/EditController.php
@@ -21,31 +21,34 @@ function getItemContent($itemType, $itemId) {
 }
 
 
-function builItemForm($item, $path)  {
+function builItemForm($item, $path, $type)  {
 
-    $html = "<form action='$path' name='something'  method='post'>";
+    $html = '<form method="post" enctype="multipart/form-data" action="../Controllers/SubmitController.php">';
     foreach ($item[0] as $key => $data) {
         $html .= "<div class='form-group'>";
         if(strlen($data) > 150) {
 
             $html.= "<h1>$key</h1>";
-            $html.= "<textarea id='$key' rows='15' cols='90'>$data</textarea>";
+            $html.= "<textarea id='$key' name='$key'rows='15' cols='90'>$data</textarea>";
 
         } else if(strpos($data, 'jpg') || strpos($data, 'jpeg') || strpos($data, 'png')) {
             $html.="<h4>$key</h4>";
             $html.="<h6>Current Image</h6>";
             $html.="<img src='$data'>";
             $html.="<h6>Change Image<h6>";
-            $html .= "<input style=''class='btn btn-default' type='file' name='' id='$key'>";
+            //$html .= "<input style='' class='btn btn-default' type='file' name='$key' id='$key'>";
+
+            $html .= "<input type='file' name='pictures[]' multiple='true'>";
         }
         else {
             $html.="<label>$key</label>";
-            $html.="<input value='$data' type='text' class='form-control'id=''>";
+            $html.="<input name='$key' value='$data' type='text' class='form-control'id=''>";
 
         }
         $html .= "</div>";
 
     }
+    $html.="<input style='display:none' name='form-type' value='$type' type='text' class='form-control'id=''>";
     $html .= "<input type='submit' value='Submit'>";
     $hmtl .= "</form>";
     print_r($html);
@@ -57,4 +60,4 @@ function persistUpdateItem() {
 }
 
 $news = getItemContent("news", 100);
-builItemForm($news, "../Controllers/SubmitController.php");
+builItemForm($news, "../Controllers/SubmitController.php", "news");
--- dist/utils/chameleon-pannel/Dashboard/Controllers/SubmitController.php
+++ dist/utils/chameleon-pannel/Dashboard/Controllers/SubmitController.php
@@ -1,3 +1,23 @@
 <?php
 
-var_dump($_POST);
+
+
+
+function uploadFile($image, $DashboardPath, $publicPath)
+foreach ($_FILES['pictures']['error'] as $key => $error) {
+    if ($error == UPLOAD_ERR_OK) {
+
+        $tmpName = $_FILES['pictures']['tmp_name'][$key];
+        $tmpNameToClient = $_FILES['pictures']['tmp_name'][$key];
+        // basename() may prevent directory traversal attacks, but further
+        // validations are required
+        $name = basename($_FILES['pictures']['name'][$key]);
+
+        move_uploaded_file($tmpName, "$DashboardPath/$name");
+        copy("$DashboardPath/$name", "$publicPath/$name");
+
+
+    }
+}
+
+    ?>
