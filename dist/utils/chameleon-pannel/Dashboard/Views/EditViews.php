a19d2fe72a68327ae7583a9c04900635a9be1cc6
antoinefortin <antoineradiohst@gmail.com>
2018-02-13 14:09:00 -0500

    Adding CKE Editor beautiful editor

--- dist/utils/chameleon-pannel/Dashboard/Views/EditViews.php
+++ dist/utils/chameleon-pannel/Dashboard/Views/EditViews.php
@@ -1,1 +1,9 @@
+<head>
+    <script src="../Assets/CKE/ckeditor.js"/></script>
+</head>
+
 <?php require_once "../Controllers/EditController.php"; ?>
+
+<script type="text/javascript">
+     CKEDITOR.replace( 'paragraphes' );
+</script>
